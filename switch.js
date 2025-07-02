7208d8a3728479b4a11f9af37ec6346b7f25d291
IvanAram <ivan.ags.95@gmail.com>
2025-07-02 15:15:22 -0600

    added switch

---
+++ switch.js
@@ -0,0 +1,41 @@
+
+
+// const num = 10;
+
+// if(num === 0) {
+//   console.log("Entered to num === 0");
+// } else if (num === 1){
+//   console.log("Entered to num === 1");
+// } else if (num === 2){
+//   console.log("Entered to num === 2");
+// } else if (num === 3){
+//   console.log("Entered to num === 3");
+// } else if (num === 4){
+//   console.log("Entered to num === 4");
+// } else if (num === 5){
+//   console.log("Entered to num === 5");
+// }
+
+// switch (num) {
+//   case 0:
+//     console.log("Entered to case 0");
+//     break;
+//   case 1:
+//     console.log("Entered to case 1");
+//     break;
+//   case 2:
+//     console.log("Entered to case 2");
+//     break;
+//   case 3:
+//     console.log("Entered to case 3");
+//     break;
+//   case 4:
+//     console.log("Entered to case 4");
+//     break;
+//   case 5:
+//     console.log("Entered to case 5");
+//     break;
+//   default:
+//     console.log("Entered default");
+//     break;
+// }
