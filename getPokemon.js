42e495ed009a081edb46f8482596c484fdcabcef
IvanAram <ivan.ags.95@gmail.com>
2025-08-12 10:40:29 -0600

    chagnes

--- getPokemon.js
+++ getPokemon.js
@@ -12,8 +12,26 @@ function getPokemonWithPromise(pokemon) {
       throw new Error(`Response status: ${response.status}`);
     }
     return response.json();
+  }).then(json => {
+    console.log(json);
+  }).catch(err => {
+    console.log(err);
   });
 }
 
+async function getPokemonAsync(pokemon) {
+  try {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const response = await fetch(url);
+    if(!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-getPokemonWithPromise(pokemonName); 
+getPokemonWithPromise(pokemonName);
+getPokemonAsync(pokemonName);
